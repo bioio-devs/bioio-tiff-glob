19f4d83307b2469b32ae92c24a716d1e831d937d
BrianWhitneyAI <brian.whitney@alleninstitute.org>
2023-08-16 11:19:25 -0700

    cookiecutter commit

---
+++ bioio_tiff_glob/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for bioio_tiff_glob."""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("bioio-tiff-glob")
+except PackageNotFoundError:
+    __version__ = "uninstalled"
+
+__author__ = "bioio-devs"
+__email__ = "brian.whitney@alleninstitute.org"
+
+from .reader_metadata import Reader
+from .reader_metadata import ReaderMetadata
+
+__all__ = ["Reader", "ReaderMetadata"]
